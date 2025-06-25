214b483448b5e7b035c17c061e5e290eb6bfdffd
no1Worst <virgelpersonal@gmail.com>
2025-06-25 09:51:24 +0800

    Add files via upload

---
+++ doyoulikemepo.java
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".bo-btn")
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Ackkkkkkkkk, I love you too love!";
+    gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height:
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.syle.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
